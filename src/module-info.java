7db7a1fa3b01ed828579e8baae4d62ab3fc55bf0
acalabro <acalabro@X1>
2021-02-07 20:09:56 +0100

    First test

---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+module glimpseBiecoCanProbe {
+	requires com.fasterxml.jackson.annotation;
+	requires com.fasterxml.jackson.core;
+	requires com.fasterxml.jackson.databind;
+	requires activemq.all;
+	requires java.naming;
+	requires java.management;
+}

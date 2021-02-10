ba24fa62698e3c50de418a252a56e85a2ae974df
acalabro <acalabro@X1>
2021-02-10 20:06:50 +0100

    p

---
+++ src/it/cnr/isti/labsedc/concern/event/ConcernAbstractEvent.java
@@ -0,0 +1,63 @@
+package it.cnr.isti.labsedc.concern.event;
+
+import java.io.Serializable;
+
+import it.cnr.isti.labsedc.concern.cep.CepType;
+
+public abstract class ConcernAbstractEvent<T> implements ConcernEvent<T>, Serializable {
+
+	private static final long serialVersionUID = 7077313246352116557L;
+	private String checksum;
+	private String sender;
+	private CepType cepType;
+	private long timestamp;
+	private T data;
+
+
+	public ConcernAbstractEvent(T data,CepType type,String senderID, String checksum, long timestamp) {
+		this.setEventData(data);
+		this.setTimestamp(timestamp);
+		this.setCepType(type);
+		this.setSenderID(senderID);
+		this.setChecksum(checksum);
+	}
+
+	public T getEventData() {
+		return data;
+	}
+
+	public void setEventData(T data) {
+		this.data = data;
+	}
+
+	public long getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(long timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public CepType getCepType() {
+		return cepType;
+	}
+
+	public void setCepType(CepType cepType) {
+		this.cepType = cepType;
+	}
+
+	public String getSenderID() {
+		return sender;
+	}
+
+	public void setSenderID(String sender) {
+		this.sender = sender;
+	}
+
+	public String getChecksum() {
+		return checksum;
+	}
+
+	public void setChecksum(String checksum) {
+		this.checksum = checksum;
+	}
+}
